0dfbdec843c525995f7ba2c202fbd614fd835338
Maxim Bodrak <kilbirdih@gmail.com>
2016-03-28 00:54:02 +0500

    поменял дату, и файлы перекинул в Java папку

--- True/src/main/java/CRUD.java
+++ True/src/main/java/CRUD.java
@@ -1,7 +1,9 @@
 import org.hibernate.Session;
+import unt.HibernateUtil;
 import unt.UserEntity;
 
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CRUD {
 
@@ -10,19 +12,19 @@ public class CRUD {
     public void createUser(String name, int age, int isAdmin)  {
         session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
-        Calendar calendar = Calendar.getInstance();
+        Date date = new Date();
         UserEntity user = new UserEntity();
        // user.setId(id);
         user.setName(name);
         user.setAge(age);
         user.setIsAdmin(isAdmin);
-        user.setCreatedData(calendar.toString());
+        user.setCreatedData(new SimpleDateFormat("dd.mm.yyyy").format(date));
         session.save(user);
         session.getTransaction().commit();
     }
 
     private void deleteUser(Object o) {
-         session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
         session.delete(o);
         session.flush();
